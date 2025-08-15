abe581ddb08083509f5641669e69b5c06382d61e
NARAIN <am.sc.u4cys25027@am.students.amrita.edu>
2025-08-15 23:16:15 +0530

    1

--- Basic_input_with_arithmatics/4.AREA_OF_RECTANGLE.py
+++ Basic_input_with_arithmatics/4.AREA_OF_RECTANGLE.py
@@ -1,4 +1,5 @@
 length=eval(input('Enter the length of the rectangle:'))
 breadth=eval(input('Enter the breadth of the rectangle:'))
+#FORMULA OF AREA OF RECTANGLE
 area_rec=length*breadth
 print('Area of the rectangle:',area_rec)
--- Basic_input_with_arithmatics/5.Celsius_to_Fahrenheit.py
+++ Basic_input_with_arithmatics/5.Celsius_to_Fahrenheit.py
@@ -1,3 +1,4 @@
 celsius=eval(input('Enter the degree celsius:'))
+#FORMULA FOR CONVERTING CELSIUS TO FAHRENHEIT
 fahren=(celsius*(9/5))+32
 print(f'degree {celsius} celsius is {fahren} farenheit')
